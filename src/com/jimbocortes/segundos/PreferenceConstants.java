2d1aefae437f0c988a0661b63b54bef6052ecf27
Jimbo Cortes <jimbo.cortes@gmail.com>
2011-10-29 21:57:32 +0800

    - added constants for application settings - modified id of textTime - initialized shared preferences

---
+++ src/com/jimbocortes/segundos/PreferenceConstants.java
@@ -0,0 +1,16 @@
+package com.jimbocortes.segundos;
+
+public class PreferenceConstants {
+	public static final String PREFERENCE_NAME = "SegundosPrefs";
+	
+	public static final String PREFERENCE_NO_OF_SETS = "numberOfSets";
+	public static final String PREFERENCE_COUNTDOWN_TIME = "countdownTime";
+	public static final String PREFERENCE_LOW_INTERVAL_TIME = "lowIntervalTime";
+	public static final String PREFERENCE_HIGH_INTERVAL_TIME = "highIntervalTime";
+	
+	public static final int DEFAULT_PREFERENCE_NO_OF_SETS = 10;
+	public static final int DEFAULT_PREFERENCE_COUNTDOWN_TIME = 10; // in seconds
+	public static final int DEFAULT_PREFERENCE_LOW_INTERVAL_TIME = 1;
+	public static final int DEFAULT_PREFERENCE_HIGH_INTERVAL_TIME = 2;
+
+}
--- src/com/jimbocortes/segundos/SegundosActivity.java
+++ src/com/jimbocortes/segundos/SegundosActivity.java
@@ -1,13 +1,72 @@
 package com.jimbocortes.segundos;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SegundosActivity extends Activity {
-    /** Called when the activity is first created. */
+    
+	private View mTextTimer;
+	private View mButtonPlayPause;
+	private View mButtonStop;
+	private SharedPreferences mPrefs;
+	private Editor mPrefEditor;
+	 
+	private OnClickListener sButtonPlayPauseListener = new OnClickListener() 
+	{ 	
+		@Override
+		public void onClick(View v) {
+			String currentText = ((Button)mButtonPlayPause).getText().toString();
+			String playText = getResources().getString(R.string.play);
+			String pauseText = getResources().getString(R.string.pause);
+			
+			if (currentText == playText)
+				((Button)mButtonPlayPause).setText(R.string.pause);
+			else
+				((Button)mButtonPlayPause).setText(R.string.play);
+		}
+	};
+	
+	private OnClickListener sButtonStopListener = new OnClickListener() 
+	{ 	
+		@Override
+		public void onClick(View v) {
+			
+		}
+	}; 
+ 
+	private void setMembers()
+    {
+    	mTextTimer = findViewById(R.id.textTime);
+    	mButtonPlayPause = findViewById(R.id.buttonPlayPause);
+    	mButtonStop = findViewById(R.id.buttonStop);
+    	
+    	mButtonPlayPause.setOnClickListener(sButtonPlayPauseListener);
+    }
+    
+    private void setPreferences()
+    {
+    	mPrefs = getSharedPreferences(PreferenceConstants.PREFERENCE_NAME, MODE_PRIVATE);
+    	if (mPrefs != null)
+    	{
+    		mPrefEditor = mPrefs.edit();
+    		mPrefEditor.putInt(PreferenceConstants.PREFERENCE_NO_OF_SETS, PreferenceConstants.DEFAULT_PREFERENCE_NO_OF_SETS);
+    		mPrefEditor.putInt(PreferenceConstants.PREFERENCE_COUNTDOWN_TIME, PreferenceConstants.DEFAULT_PREFERENCE_COUNTDOWN_TIME);
+    		mPrefEditor.putInt(PreferenceConstants.PREFERENCE_LOW_INTERVAL_TIME, PreferenceConstants.DEFAULT_PREFERENCE_LOW_INTERVAL_TIME);
+    		mPrefEditor.putInt(PreferenceConstants.PREFERENCE_HIGH_INTERVAL_TIME, PreferenceConstants.DEFAULT_PREFERENCE_HIGH_INTERVAL_TIME);
+    	}
+    }
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        setMembers();
+        setPreferences();
     }
 }
