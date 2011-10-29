28d3ec0dd36303c46f0a59bee76dec0fdcaffba9
Jimbo Cortes <jimbo.cortes@gmail.com>
2011-10-29 18:05:09 +0800

    first commit to greatness XD

---
+++ src/com/jimbocortes/segundos/SegundosActivity.java
@@ -0,0 +1,13 @@
+package com.jimbocortes.segundos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SegundosActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
